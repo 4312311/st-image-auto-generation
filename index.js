71b50c68d8c4c60082a36980fd623887cf1e2b63
4312311 <4312311@users.noreply.github.com>
2025-10-01 23:08:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -337,7 +337,7 @@ async function handleIncomingMessage() {
                             // Find the original image tag in the message
                             const originalTag = message.mes.match(imgTagRegex)[0];
                             // Replace it with an actual image tag
-                            const newImageTag = `<img src="${imageUrl}" >`;
+                            const newImageTag = `<img src="${imageUrl}" prompt="${prompt}" >`;
                             message.mes = message.mes.replace(originalTag, newImageTag);
 
                             // Update the message display using updateMessageBlock
