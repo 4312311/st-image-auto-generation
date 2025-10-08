48d9c666ff816860a84f00436822114503fcde6d
4312311 <4312311@users.noreply.github.com>
2025-10-08 16:39:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -343,7 +343,7 @@ async function handleIncomingMessage() {
 
     // 使用正则表达式search
    // const imgTagRegex = regexFromString(extension_settings[extensionName].promptInjection.regex);
-const imgTagRegex = /<\s*img\b(?![^>]*current_datetime="[^"]*2025100522[^"]*")(?=[^>]*prompt="([^"]*)")[^>]*>/g;
+const imgTagRegex = /<[\s\r\n]*img(?!(?:(?!>).)*?current_datetime\s*=\s*"[^"]*2025100522[^"]*")[^>]*?prompt\s*=\s*"([^"]*)"[^>]*?>/gis;
 	let matches = imgTagRegex.global ? [...message.mes.matchAll(imgTagRegex)].map(match => match[1]) : [message.mes.match(imgTagRegex)[1]]; // 只取捕获组的内容
     console.log(imgTagRegex, matches)
 
